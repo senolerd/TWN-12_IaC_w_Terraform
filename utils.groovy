d5db4d03093a13d0927cd1db8a45d6efffd31a05
Senol Erdogan <senol@mac.local>
2026-08-25 13:02:59 -0500

    podman login added to nested plugins

--- utils.groovy
+++ utils.groovy
@@ -47,22 +47,28 @@ def iacDeploy(){
                 terraform validate 
                 terraform apply -auto-approve -var="keypair_name=${env.EC2_KEY_PAIR_NAME}" -var="region=${env.REGION}"
             """)
-            env.EC2_SERVER_IP = sh(script:"terraform output server_addr", returnStdout: true).trim()
+            EC2_SERVER_IP = sh(script:"terraform output server_addr", returnStdout: true).trim()
         }
     }
 }
 
-
 def sshWork() {
-    echo "SERVER ADDRESS TO SSH: ${env.EC2_SERVER_IP}"
+    echo "[sshWork]: SERVER ADDRESS TO SSH: ${EC2_SERVER_IP}"
 
-    echo "90 sec sleep before asking podman"
+    echo "[sshWork]: 90 sec sleep before asking podman to do something"
     sleep(time: 90, unit: 'SECONDS')
     
     sshagent(credentials: ['ec2_pem_for_ubuntu']) {
-        sh("ssh -o StrictHostKeyChecking=no ubuntu@${env.EC2_SERVER_IP} podman image ls")
+        withCredentials([usernamePassword(credentialsId: 'dockerhub-pat-rw', passwordVariable: 'DOCKER_PW', usernameVariable: 'DOCKER_USER')]) { 
+            
+            echo "[sshWork]: Checking podman whether it answers"
+            sh "ssh -o StrictHostKeyChecking=no ubuntu@${EC2_SERVER_IP} podman image ls"
+
+            echo "[sshWork]: Trying to login docker.io"
+            sh 'ssh -o StrictHostKeyChecking=no ubuntu@${EC2_SERVER_IP} "podman login ${OCI_REG_ADDR} -u $DOCKER_USER -p $DOCKER_PW"'
+
+        }
     }
-    
 }
 
 
